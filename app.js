62d61f15c5419f5e8100d0f08ca67674cd2849ab
Stefan Zheng <zhengstefan@gmail.com>
2020-12-25 17:18:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+var favicon = require('serve-favicon')
+var path = require('path')
+
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.static("lib"));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon_package_v0.16', 'favicon.ico')));
+
+
+app.set('view engine', 'ejs');
+
+/* Footer Year */
+
+const thisYear = new Date().getFullYear();
+
+
+/* Google Total Reviews Widget */
+
+const url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJr99LYO8gvUcRrfxkVQhwB7c&fields=name,rating,user_ratings_total&key=AIzaSyCzEX4-HXN48x52r3edZhBz1XXd_Wuk1xQ"
+
+app.get("/", function (req, res) {
+    https.get(url, function (response) {
+        response.on("data", function (data) {
+            const totalReviewData = JSON.parse(data);
+
+            const name = totalReviewData.result.name;
+            const rating = totalReviewData.result.rating;
+            const userRatingsTotal = totalReviewData.result.user_ratings_total;
+
+
+
+            res.render('index', {
+                rating: rating,
+                userRatingsTotal: userRatingsTotal,
+                year: thisYear
+            })
+
+        });
+
+    })
+})
+
+app.get("/speisekarte", function (req, res) {
+    res.render('speisekarte', {
+        year: thisYear
+    });
+});
+
+app.get("/restaurant", function (req, res) {
+    res.render('restaurant', {
+        year: thisYear
+    });
+});
+
+app.get("/impressum", function (req, res) {
+    res.render('impressum', {
+        year: thisYear
+    });
+});
+
+app.get("/datenschutz", function (req, res) {
+    res.render('datenschutz', {
+        year: thisYear
+    });
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port 3000.");
+})
+
